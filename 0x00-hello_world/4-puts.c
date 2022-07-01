8cf15b5e64178e3a14f5b910b536790b38a810a8
Godwinbobby <giztherapper@gmail.com>
2022-07-01 06:48:04 -0700

    hh

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,18 @@
 #!/bin/bash
-puts("Programming is like building a multilingual puzzle);
+include<stdio.h>
+
+/**
+ *Description: Main - Prints out a string to stdout
+ *
+ * Return: 0 if success
+ * */
+
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
