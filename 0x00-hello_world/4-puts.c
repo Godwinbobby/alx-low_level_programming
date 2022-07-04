9d5a18cb170e758fa9a8b8f108cf03b16432ef54
Godwinbobby <giztherapper@gmail.com>
2022-07-04 02:36:01 -0700

    print without prinf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,13 @@
-#!/bin/bash
-include<stdio.h>
+#include<stdio.h>
 
 /**
- *Description: Main - Prints out a string to stdout
- *
- * Return: 0 if success
- * */
+ * Main - Entry point
+ * Description: prints "\"Programming is like building a multilingual puzzle"
+* Return: 0
+*/
 
 int main(void)
-
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
-
+	return(0);
 }
