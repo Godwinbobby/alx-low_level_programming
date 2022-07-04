0d868f61eb7e9c4cd3133add9591b06729e7c770
Godwinbobby <giztherapper@gmail.com>
2022-07-04 06:08:17 -0700

    gg

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,12 +3,12 @@
 /**
  * main -Print a string
  * Description: Using printf
- * return: 0
+ * Return: 0
  */
 
 int main(void) 
 {
-	printf("%s"," with proper grammar, but the outcome is a piece of art,/n");
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
 		return (0);
 }
 
